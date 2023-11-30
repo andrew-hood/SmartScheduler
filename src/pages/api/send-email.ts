a0b224d8692517e604be15946849ca2b074d157a
Paulo Alexandre Da Cunha <paulo.dacunha@go1.com>
2023-11-30 03:28:46 +0000

    Add env var for sendgrid

--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -1,22 +1,21 @@
-
-import type { NextApiRequest, NextApiResponse } from 'next';
-import sgMail from '@sendgrid/mail';
-const SENDGRID_API_KEY = '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'
-sgMail.setApiKey(SENDGRID_API_KEY);
+import type { NextApiRequest, NextApiResponse } from "next";
+import sgMail from "@sendgrid/mail";
+import { env } from "../../env.mjs"; // Import the env object
+sgMail.setApiKey(env.SENDGRID_API_KEY);
 
 async function sendEmailWithICS(icsData: string, recipientEmail: string) {
   const msg = {
     to: recipientEmail,
     //from: 'paulo.dacunha@go1.com',
-    from: 'pcunha03@gmail.com',
-    subject: 'Scheduled Learning Session',
-    text: 'A dedicated time block for learning.',
+    from: "pcunha03@gmail.com",
+    subject: "Scheduled Learning Session",
+    text: "A dedicated time block for learning.",
     attachments: [
       {
-        content: Buffer.from(icsData).toString('base64'),
-        filename: 'scheduled_learning.ics',
-        type: 'text/calendar',
-        disposition: 'attachment',
+        content: Buffer.from(icsData).toString("base64"),
+        filename: "scheduled_learning.ics",
+        type: "text/calendar",
+        disposition: "attachment",
       },
     ],
   };
@@ -24,22 +23,25 @@ async function sendEmailWithICS(icsData: string, recipientEmail: string) {
   await sgMail.send(msg);
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
     try {
       const { icsData, email } = req.body;
       await sendEmailWithICS(icsData, email);
-      res.status(200).json({ message: 'Email sent successfully' });
+      res.status(200).json({ message: "Email sent successfully" });
     } catch (error) {
       console.error(error);
       if (error instanceof Error) {
         res.status(500).json({ error: error.message });
       } else {
-        res.status(500).json({ error: 'An unknown error occurred' });
+        res.status(500).json({ error: "An unknown error occurred" });
       }
     }
   } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
 }
